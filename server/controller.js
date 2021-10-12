416e4a4c44057bacb00ff280f4b1c6471eb144f9
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-12 11:35:39 -0700

    updated ratings

--- server/controller.js
+++ server/controller.js
@@ -10,7 +10,7 @@ module.exports = {
         const {title, rating, imageURL} = req.body
         const newMovie = {
             title,
-            rating,
+            rating: +rating,
             imageURL,
             id: globalId
         }
