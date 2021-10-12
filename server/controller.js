8281dac9ba6914f5f42d3b2a6d3143729ceea178
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-12 10:50:41 -0700

    updated

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,23 @@
 const movies = require('./db.json');
+let globalId = 11
 
 module.exports = {
-    getAllMovies: () => {}
-    
+    getAllMovies: (req, res) => {
+    res.status(200).send(movies)
+    },
+    createMovie: (req, res) => {
+        //console.log(req.body)
+        const {title, rating, imageURL} = req.body
+        const newMovie = {
+            title,
+            rating,
+            imageURL,
+            id: globalId
+        }
+
+        movies.push(newMovie)
+        console.log(movies)
+        res.status(200).send(movies)
+        globalId++
+    }
 };
