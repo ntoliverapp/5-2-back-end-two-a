35a204068f57f1eaf892f5bb302ee692b9eed4c7
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-12 11:26:30 -0700

    added update and delete movie function

--- server/controller.js
+++ server/controller.js
@@ -15,9 +15,41 @@ module.exports = {
             id: globalId
         }
 
-        movies.push(newMovie)
-        console.log(movies)
+        movies.push(newMovie);
+        console.log(movies);
+        res.status(200).send(movies);
+        globalId++;
+    },
+    updateMovie: (req, res) =>{
+        // console.log(req,body);
+        // console.log(req.params);
+        const {id} = req.params
+        const {type} = req.body
+
+        let index = movies.findIndex((elem) => +elem.id === +id)
+
+        if(movies[index].rating === 5 && type === 'plus') {
+            res.status(400).send('Cannot go above 5')
+        } else if(movies[index].rating === 1 && type ==='minus'){
+            res.status(400).send('Cannot go below 1')
+        } else if(type === 'plus'){
+            movies[index].rating++
+            res.status(200).send(movies)
+        } else if (type === 'minus'){
+            movies[index].rating--
+            res.status(200).send(movies)
+        } else {
+            res.status(400).send('Something went wrong')
+        }
+
+
+    },
+    deleteMovie: (req, res) => {
+        const {id} = req.params
+
+        let index = movies.findIndex((elem) => +elem.id=== +id)
+
+        movies.splice(index, 1)
         res.status(200).send(movies)
-        globalId++
     }
 };
